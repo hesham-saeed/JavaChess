66b166e4eb06ba717d777be248b2a28958dbb3bb
Yusuf Mamdouh Awad <yusuf_awad@hotmail.com>
2018-05-06 23:35:38 +0200

    Networks base classes

---
+++ src/com/chess/engine/network/Server.java
@@ -0,0 +1,31 @@
+package com.chess.engine.network;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+
+public class Server extends NetworkConnection {
+
+    private int port;
+
+    public Server(int port, Consumer<Serializable> onRecieveCallback) {
+        // calls constructor of NetworkConnection
+        super(onRecieveCallback);
+        this.port = port;
+    }
+
+    @Override
+    protected boolean isServer() {
+        return true;
+    }
+
+    @Override
+    protected String getIP() {
+        return null;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+}
+
